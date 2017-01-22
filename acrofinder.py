fe345e8a3fb493d792e6ea2e52e6a4eaacc6f2ae
Thomas Meschede <Thomas.Meschede@tu-berlin.de>
2017-01-22 14:56:56 +0100

    acrofinder now works with csv as default and produces latex-lists

--- acrofinder.py
+++ acrofinder.py
@@ -7,26 +7,37 @@
 import re #regular expressions to search and replace acronyms
 import sys
 from collections import defaultdict
+import csv
 #import getopt
 
-repl = "\\acro{}"
+#repl = "\\acro{}"
 
-acrolist = re.compile("{(?P<a>.*)}{(?P<b>.*)}")
+#acrolist = re.compile("{(?P<a>.*)}{(?P<b>.*)}")
 
-with open("acronyms.tex") as acrofile:
-    acrostr = acrofile.read()
+def buildlatexlist(acrolist):
+    latac=r"\begin{acronym}\n"
+    for ac in acrolist:
+        latac+=r"\acro{"+ac[0]+"}{"+ac[1]+"}"+"\n"
+    latac+=r"\end{acronym}"
+    return latac
+
+with open("acronymlist.csv", "rb") as acrofile:
+    acronyms = csv.reader(acrofile, skipinitialspace=True, delimiter=',')#, quotechar='"')
+    acronyms=list(acronyms)
     
-acrostr = acrostr.split("\n")[1:-2]
-acros = [acrolist.search(i).groups() for i in acrostr]
+#acrostr = acrostr.split("\n")[1:-2]
+#acros = [acrolist.search(i).groups() for i in acrostr]
 
-#open latex file
+acros = [ac[:2] for ac in acronyms]
+
+#open file
 filename = sys.argv[1]
-with open(filename) as texfile:
-    texstr = texfile.read()
+with open(filename) as textfile:
+    textstr = textfile.read()
     
-#TODO:  if line starts with "\section"  dont replace!!    
+##TODO:  if line starts with "\section"  dont replace!!    
 
-newtex = texstr
+newtex = textstr
 counter=defaultdict(int)
 for acro, acrolong in acros:
     repl = r'\\ac{{{}}}'.format(acro)
@@ -37,30 +48,31 @@ for acro, acrolong in acros:
     #if n>0: print("replacements for {}: {}".format(regex,n))
     if n > 0: counter[acro] += n
 
-print(newtex)
+with open("acronyms.tex","w") as acrotex:
+    acrotex.write(buildlatexlist(acros))
 
 with open("acro.log","a") as acrolog:
     acrolog.write("found: {}\n".format(counter))
 #print("replacing acros in file: "+ newtex)
 #print("found: {}".format(counter))
 
-#class Usage(Exception):
-    #def __init__(self, msg):
-        #self.msg = msg
+##class Usage(Exception):
+    ##def __init__(self, msg):
+        ##self.msg = msg
 
-#def main(argv=None):
-    #if argv is None:
-        #argv = sys.argv
-    #try:
-        #try:
-            #opts, args = getopt.getopt(argv[1:], "h", ["help"])
-        #except getopt.error, msg:
-             #raise Usage(msg)
-        ## more code, unchanged
-    #except Usage, err:
-        #print >>sys.stderr, err.msg
-        #print >>sys.stderr, "for help use --help"
-        #return 2
+##def main(argv=None):
+    ##if argv is None:
+        ##argv = sys.argv
+    ##try:
+        ##try:
+            ##opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        ##except getopt.error, msg:
+             ##raise Usage(msg)
+        ### more code, unchanged
+    ##except Usage, err:
+        ##print >>sys.stderr, err.msg
+        ##print >>sys.stderr, "for help use --help"
+        ##return 2
 
-#if __name__ == "__main__":
-    #sys.exit(main())
+##if __name__ == "__main__":
+    ##sys.exit(main())
